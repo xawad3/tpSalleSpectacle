d22423de0869ed99b067a8ef0aeae600dd15a5b6
Zeshooter <romainsoulie1973@gmail.com>
2022-03-23 09:09:40 +0100

    Models Ok

--- SalleSpectacle/reservation/models.py
+++ SalleSpectacle/reservation/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+import datetime
+from django.contrib.auth.models import User
 # Create your models here.
+
+class TypeOfEvents(models.Model):
+    type_of_event_name = models.CharField(max_length=50)
+
+class Artists(models.Model):
+    artist_name = models.CharField(max_length=30)
+    artist_firstname = models.CharField(max_length=30)
+
+class WhoIsHere(models.Model):
+    who_is_here_name = models.CharField(max_length=30)
+    have = models.ManyToManyField(Artists)
+
+class Schedule(models.Model):
+    start_date = models.DateTimeField('date de début')
+    end_date = models.DateTimeField('date de fin')
+
+class TyOfShowroom(models.Model):
+    type_of_showroom_name = models.CharField(max_length=50)
+
+class Events(models.Model):
+    event_name = models.CharField(max_length=50)
+    event_start_date = models.DateTimeField('date de début')
+    event_end_date = models.DateTimeField('date de fin')
+    type_of_event = models.ForeignKey(TypeOfEvents, on_delete=models.CASCADE)
+    book = models.ManyToManyField(User)
+    perform = models.ManyToManyField(WhoIsHere)
+    prepare = models.ManyToManyField(Schedule)
+
+class Showroom(models.Model):
+    showroom_name = models.CharField(max_length=30)
+    showroom_places = models.IntegerField(default=0)
+    events = models.ForeignKey(Events, on_delete=models.CASCADE)
+    tyofshowroom = models.ForeignKey(TyOfShowroom, on_delete=models.CASCADE)
---
+++ SalleSpectacle/reservation/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'reservation'
+
+urlpatterns= [
+    path('', views.index, name='index'),
+    # path('<int:pk>/', views.the_event.as_view(), name='the_event'),
+
+]
